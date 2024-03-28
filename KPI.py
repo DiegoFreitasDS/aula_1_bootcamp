e3fc22bcf4d2e7dfb83cd3a9ac5396a98c8bf6b7
DiegoFreitasDS <diegop.freitas@gmail.com>
2024-03-28 16:24:53 -0300

    Update README

--- KPI.py
+++ KPI.py
@@ -1,37 +1,18 @@
-# Escreva um programa em Python que solicita ao usuário para digitar seu nome, o valor do seu salário mensal e o valor do bônus que recebeu. O programa deve, então, 
-# imprimir uma mensagem saudando o usuário pelo nome e informando o valor do salário em comparação com o bônus recebido.
-
-# Instruções:
-# 1 O programa deve começar solicitando ao usuário que insira seu nome.
+# Solicitar o nome do usuário
 print('Olá, tudo bem?')
 nome = input("\nQual o seu nome? \n")
 
-# 2 Em seguida, o programa deve pedir ao usuário para inserir o valor do seu salário. Considere que este valor pode ser um número decimal.
-
+# Solicitar o salário do usuário
 salario = float(input(f'\n{nome}, digite seu salário: \n'))
 
-# 3 Depois, o programa deve solicitar a porcentagem do bônus recebido pelo usuário, que também pode ser um número decimal.
-
+# Solicitar a % do bônus do usuário
 bonus = float(input("\nAgora, digite a % do seu bonus: \n"))
 
-# 4 O cálculo do KPI do bônus de 2024 é de 1000 + salario * bônus
-
+# Comissão fixa
 fixo = 1000
 
+# Cálculo do KPI
 calculo = fixo + (salario*bonus)
 
-# 5 Finalmente, o programa deve imprimir uma mensagem no seguinte formato: "Olá [nome], o seu valor bônus foi de 5000".
-
+# Devolução do valor calculado para o usuário
 print(f'\nOlá {nome}, o seu valor bônus foi de R$ {calculo:.2f}.')
-
-# Exemplo de Saída:
-# Se o usuário digitar "Luciano" como nome, "5000" como salário e "1.5" como bônus, o programa deve imprimir:
-
-
-# Olá Luciano, o seu bônus foi de 8500
-
-
-# 6 Salve esse script python como kpi.py
-# 7 Faça uma documentação simples de como executar esse programa, utilize o README
-# 8 Salve no Git e no Github
-# 9 Isso ajudará a consolidar seu conhecimento sobre o Git e a familiarizar-se com o processo de versionamento de código.
--- README.md
+++ README.md
@@ -1,3 +1,16 @@
-# aula_1_bootcamp
-# aula_01_bootcamp
-# aula_1_bootcamp
+# CRIAÇÃO DE KPI SIMPLES
+
+Nesse primeiro projeto, fiz um KPI simples para cálculo *automático* de Bônus do Funcionário. 
+
+Seu Funcionamento é simples, porém **intuitivo e preciso**.
+
+O Programa irá solicitar:
+
+1. *O Nome do Usuário*
+2. *O Seu Salário*
+3. *Qual o valor da sua comissão*
+
+![Link da imagem](image-1.png)
+
+
+Após estes inputs, o programa irá calcular o bônus **automaticamente**, tendo em vista que implicito no código, há um valor de comissão fixo, que é somado ao bônus variável.
